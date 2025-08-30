47b03c969ce14ae04aee69763e336814369987d9
akiijadhav <akiijadhav22@gmail.com>
2025-08-31 04:21:22 +0530

    test: add test case for newline delimiters

--- __tests__/add.test.js
+++ __tests__/add.test.js
@@ -18,4 +18,8 @@ describe("String Calculator - add(string)", () => {
         expect(add("1,2,3,4,5")).toBe(15);
         expect(add("10,20,30")).toBe(60);
     });
+
+    test("accepts newlines as alternative delimiters to commas and returns sum", () => {
+        expect(add("1\n2,3")).toBe(6);
+    });
 });
