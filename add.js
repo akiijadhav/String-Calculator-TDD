15c448e89c5f715f4c0a59515e039d75477a99b0
akiijadhav <akiijadhav22@gmail.com>
2025-08-31 05:08:32 +0530

    feat: throw error when negative numbers are passed

--- add.js
+++ add.js
@@ -3,6 +3,7 @@
  *
  * @param {string} numbersString - A string of comma-separated numbers (e.g., "1,2,3").
  * @returns {number} The sum of the numbers. Returns 0 if the input string is empty.
+ * @throws {Error} When negative numbers are found in the input.
  *
  * @example
  * add("");      // 0
@@ -10,10 +11,12 @@
  * add("1,5");   // 6
  * add("1\n2,3"); // 6
  * add("//;\n1;2"); // 3
+ * add("1,-2,3"); // throws Error: "negative numbers not allowed -2"
+ * add("1,-2,3,-4"); // throws Error: "negative numbers not allowed -2,-4"
  */
 
 // function to add numbers from a string
-function add(numbersString)  {
+function add(numbersString) {
     // return 0 for empty string
     if (numbersString === "") return 0;
 
@@ -40,6 +43,12 @@ function add(numbersString)  {
         .split(delimiterRegex)
         .map(num => parseInt(num))
         .filter(num => !isNaN(num));
+
+    // Check for negative numbers & throw error if any found
+    const negativeNumbers = numbers.filter(num => num < 0);
+    if (negativeNumbers.length > 0) {
+        throw new Error(`negative numbers not allowed ${negativeNumbers.join(",")}`);
+    }
 
     // return the sum of the numbers
     return numbers.reduce((sum, num) => sum + num, 0);
